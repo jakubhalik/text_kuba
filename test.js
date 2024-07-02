6df42b450352a23ec18227d184af10b200807044
Jakub Halik <jakubhalik1@gmail.com>
2024-07-02 09:38:39 +0200

    db connection works

---
+++ test.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+const readlineSync = require('readline-sync');
+
+// Prompt for the password without showing input
+const password = readlineSync.question(
+    'Enter the password for the database: ',
+    {
+        hideEchoBack: true,
+    }
+);
+
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    database: 'text_kuba',
+    user: 'kuba',
+    password: password,
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(res.rows);
+    pool.end();
+});
