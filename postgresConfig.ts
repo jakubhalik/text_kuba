71e9467465312a8d78c8b39ef74c72aea6fc06d3
Jakub Halik <jakubhalik1@gmail.com>
2024-08-17 15:59:41 +0200

    multiple users logins and sign ups all to be happening chillliiii in the same time handled in postgresConfig.ts by giving more clients and idles

--- postgresConfig.ts
+++ postgresConfig.ts
@@ -14,6 +14,9 @@ export const postgresUserPool = new Pool({
     database: `text_${owner}`,
     user: 'postgres',
     password: postgresPassword,
+    max: 1000,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 });
 
 const postgresCombinedPassword = `postgres${postgresPassword}`;
